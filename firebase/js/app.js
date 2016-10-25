e91cf065de024617afb5a675c92c25309869a54c
Simon Bang <bangtsimon@gmail.com>
2016-10-25 12:18:16 -0700

    firebase practice

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -12,3 +12,102 @@
 //  to the URL provided by Firebase 
 
 //paste the Firebase initialization code here
+ // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB_7QeNR6OTOjTMAgxwdvq5G2wpUfrKvOA",
+    authDomain: "tasks-demo-ef8b3.firebaseapp.com",
+    databaseURL: "https://tasks-demo-ef8b3.firebaseio.com",
+    storageBucket: "tasks-demo-ef8b3.appspot.com",
+    messagingSenderId: "834063654490"
+  };
+firebase.initializeApp(config);
+  var currentUser;
+  var authProvider = new firebase.auth.GithubAuthProvider();
+  
+// This function gets called whenever the authentication state 
+// changes: "signs in or signs out".
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        currentUser = user;
+        console.log(currentUser);
+    } else {
+          // force them to go into GitHub and sign in by redirecting them to Github.
+        firebase.auth().signInWithRedirect(authProvider);
+    }
+});
+
+var taskForm = document.querySelector(".new-task-form");
+var taskTitleInput = taskForm.querySelector(".new-task-title");
+var taskList = document.querySelector(".task-list");
+var purgeButton = document.querySelector(".btn-purge");
+
+var tasksRef = firebase.database().ref("tasks");
+
+taskForm.addEventListener("submit", function(evt) {
+    // keeps browser from doing default behavior
+    evt.preventDefault();
+
+    var task = {
+        title: taskTitleInput.value.trim(),
+        done: false,
+        createdOn: firebase.database.ServerValue.TIMESTAMP,
+        createdBy: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            email: currentUser.email
+        }
+    };
+    tasksRef.push(task);
+
+    taskTitleInput.value = "";
+    
+    return false;
+});
+
+function renderTask(snapshot) {
+    var task = snapshot.val();
+    var li = document.createElement("li");
+    var spanTitle = document.createElement("span");
+    spanTitle.textContent = task.title;
+    spanTitle.classList.add("task-title");
+    li.appendChild(spanTitle);
+
+    var spanCreation = document.createElement("span");
+    spanCreation.textContent = moment(task.createdOn).fromNow() + " by " + (task.createdBy.displayName || task.createdBy.email);
+    spanCreation.classList.add("task-creation");
+    li.appendChild(spanCreation);
+
+    if (task.done) {
+        li.classList.add("done");
+        purgeButton.classList.add("hidden");
+    }
+
+    li.addEventListener("click", function() {
+       // console.log("click for " + task.title);
+       snapshot.ref.update( {
+           done: !task.done
+       });
+    });
+
+    taskList.appendChild(li);
+}
+
+function render(snapshot) {
+    taskList.innerHTML = "";
+    purgeButton.classList.add("hidden");
+    snapshot.forEach(renderTask)
+
+}
+
+tasksRef.on("value", render);
+
+purgeButton.addEventListener("click", function() {
+    console.log("purge button clicked!");
+    tasksRef.once("value", function(snapshot) {
+        snapshot.forEach(function(taskSnapshot) {
+            if (taskSnaoshot.val().done) {
+                taskSnapshot.ref.remove();
+            }
+        })
+    });
+});
